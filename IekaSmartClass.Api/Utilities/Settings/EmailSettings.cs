89397af0ed50dcd79019614c4166deb6f6ccab8c
Juxhin Xhihani <juxhinhihani@gmail.com>
2026-04-08 01:11:48 +0200

    fix

--- IekaSmartClass.Api/Utilities/Settings/EmailSettings.cs
+++ IekaSmartClass.Api/Utilities/Settings/EmailSettings.cs
@@ -5,7 +5,7 @@ public class EmailSettings
     public const string SectionName = "Email";
 
     public string AppName { get; set; } = "IEKA SmartClass";
-    public string FrontendBaseUrl { get; set; } = "http://localhost:3000";
+    public string FrontendBaseUrl { get; set; } = "https://iekaclass.vercel.app";
     public SmtpSettings Smtp { get; set; } = new();
 }
 
